e4820a83f824590d8e69759b9813167b2c790107
Mike Kistler <mikekistler@microsoft.com>
2025-05-09 09:21:58 -0500

    Rename ApiService to Catalog

--- Demo.AppHost/Program.cs
+++ Demo.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Demo_ApiService>("apiservice")
+builder.AddProject<Projects.Demo_Catalog>("catalog")
     .WithHttpsHealthCheck("/health");
 
 builder.Build().Run();
