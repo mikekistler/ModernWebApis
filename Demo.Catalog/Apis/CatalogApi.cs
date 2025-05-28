53ec5a7d0740a1265f8167784aacbb545738ad06
Mike Kistler <mikekistler@microsoft.com>
2025-05-28 14:31:57 -0500

    Add validation

--- Demo.Catalog/Apis/CatalogApi.cs
+++ Demo.Catalog/Apis/CatalogApi.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 using Catalog.Data;
 using Catalog.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Catalog.API;
 
@@ -24,7 +25,8 @@ public static class CatalogApi
 
         api.MapGet("/items/{id:int}/pic", GetItemPictureById);
 
-        api.MapPatch("/items/{id:int}", UpdateItem);
+        // Disable patch endpoint due to known issue with Validation and JsonPatchDocument
+        // api.MapPatch("/items/{id:int}", UpdateItem);
 
         api.MapPost("/items", CreateItem);
 
@@ -101,7 +103,7 @@ public static class CatalogApi
     public static async Task<Ok<List<CatalogItem>>> GetItemsByIds(
         HttpRequest httpRequest,
         [AsParameters] CatalogServices services,
-        int[] ids)
+        [MinLength(1)] int[] ids)
     {
         // workaround for https://github.com/dotnet/aspnetcore/issues/61770
         var Context = httpRequest.HttpContext.RequestServices.GetRequiredService<CatalogContext>();
